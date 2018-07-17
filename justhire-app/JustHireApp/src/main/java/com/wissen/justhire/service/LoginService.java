3055f87e69a01db7bcd979b8a88e459e8055a59a
Komal Naik <komalnaik154@gmail.com>
2018-07-17 18:23:32 +0530

    Login Service added

---
+++ justhire-app/JustHireApp/src/main/java/com/wissen/justhire/service/LoginService.java
@@ -0,0 +1,5 @@
+package com.wissen.justhire.service;
+
+public interface LoginService {
+	boolean authenticateUser(String email, String Password);
+}
---
+++ justhire-app/JustHireApp/src/main/java/com/wissen/justhire/service/LoginServiceImpl.java
@@ -0,0 +1,38 @@
+package com.wissen.justhire.service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wissen.justhire.model.Login;
+import com.wissen.justhire.repository.LoginRepository;
+
+public class LoginServiceImpl implements LoginService {
+	@Autowired
+	private LoginRepository loginRepository;
+
+	@Override
+	public boolean authenticateUser(String email, String password) {
+		Login login = loginRepository.findByEmail(email);
+		String actualPassword = login.getPassword();
+		
+		MessageDigest md =null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        String enteredPassword = DatatypeConverter.printHexBinary(digest);
+        boolean isLogin=false;
+        if(actualPassword.equals(enteredPassword)) {
+        	isLogin = true;
+        }
+		return isLogin;
+	}
+
+}
