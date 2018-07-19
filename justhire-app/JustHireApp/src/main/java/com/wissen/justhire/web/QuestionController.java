409235d7ac4f8f66d60db9690c74751968c59364
Komal Naik <komalnaik154@gmail.com>
2018-07-19 09:53:22 +0530

    question bank

--- justhire-app/JustHireApp/src/main/java/com/wissen/justhire/web/QuestionController.java
+++ justhire-app/JustHireApp/src/main/java/com/wissen/justhire/web/QuestionController.java
@@ -1,5 +1,10 @@
 package com.wissen.justhire.web;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.wissen.justhire.model.Question;
+import com.wissen.justhire.model.Round;
 import com.wissen.justhire.model.User;
 import com.wissen.justhire.service.AdminService;
 import com.wissen.justhire.service.QuestionService;
@@ -21,13 +29,13 @@ import com.wissen.justhire.service.QuestionService;
 @RestController
 @RequestMapping(value = "/api/question")
 public class QuestionController {
-	
+
 	@Autowired
 	private QuestionService questionService;
-	
+
 	@Autowired
 	private AdminService adminService;
-	
+
 	@PostMapping(consumes = { "application/json" }, produces = { "application/json" })
 	public void addNewQuestion(@RequestBody QuestionForm form) {
 
@@ -39,14 +47,14 @@ public class QuestionController {
 		question.setRound(form.getRound());
 		questionService.addSingleQuestion(question);
 	}
-	
-	//Working
+
+	// Working
 	@GetMapping(produces = { "application/json" })
 	public List<Question> getAllQuestions() {
 		List<Question> questions = questionService.getAllQuestions();
 		return questions;
 	}
-	
+
 	@GetMapping(value = "{userId}", produces = { "application/json" })
 	public List<Question> getQuestionsForUser(@PathVariable int userId) {
 		User user = new User();
@@ -54,16 +62,15 @@ public class QuestionController {
 		List<Question> questions = questionService.getQuestions(user);
 		return questions;
 	}
-	
-	//Working
-	@PutMapping(value="{questionId}", produces = {"application/json"})
+
+	// Working
+	@PutMapping(value = "{questionId}", produces = { "application/json" })
 	public void approveQuestion(@PathVariable int questionId) {
 		adminService.approveQuestion(questionId);
 	}
-	
-	
-	@PostMapping(value="{questionId}", produces = {"application/json"})
-	public void editQuestion(@PathVariable int questionId, @RequestBody QuestionForm form ) {
+
+	@PostMapping(value = "{questionId}", produces = { "application/json" })
+	public void editQuestion(@PathVariable int questionId, @RequestBody QuestionForm form) {
 		Question question = new Question();
 		question.setQuestionId(questionId);
 		question.setQuestion(form.getQuestion());
@@ -74,5 +81,31 @@ public class QuestionController {
 		questionService.editQuestion(question);
 	}
 
+	@SuppressWarnings("resource")
+	@PostMapping(headers = ("content-type=multipart/*"), consumes = {
+			"application/x-www-form-urlencoded" }, produces = { "application/json" })
+	public void addQuestionBank(@RequestParam("file") MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		file.transferTo(convFile);
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		br = new BufferedReader(new FileReader(convFile));
+		List<Question> questions = new ArrayList<>();
+		while ((line = br.readLine()) != null) {
+			String[] values = line.split(cvsSplitBy);
+			Question question = new Question();
+			Round round = new Round();
+			question.setQuestion(values[0]);
+			question.setDifficulty(values[1]);
+			question.setExperience(values[2]);
+			question.setComment(values[3]);
+			round.setRoundNumber(Integer.parseInt(values[4]));
+			question.setRound(round);
+			questions.add(question);
+		}
+		questionService.addQuestionBank(questions);
+
+	}
 
 }
